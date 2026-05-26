356d3729a317de1b814462f4daed15f5205b39b5
Nosde Casther <edsoncastilloh@outlook.com>
2026-05-26 07:04:31 -0600

    fix: force HTTPS if APP_ENV is not local

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
+        if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
         }
 
